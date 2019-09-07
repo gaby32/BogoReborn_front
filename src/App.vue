547dd0d9c09d15f9d878fe67d6827a2dd4b0756f
gaby32 <39409503+gaby32@users.noreply.github.com>
2019-09-07 16:04:33 -0400

    yo

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,46 @@
-
-
-
 <template>
-  <div>
+    <div>
+        <v-app-bar
+                color="deep-purple accent-4"
+        >
+            <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-  </div>
+            <v-toolbar-title>Page title</v-toolbar-title>
 
+            <div class="flex-grow-1"></div>
+
+            <v-btn icon>
+                <v-icon>mdi-heart</v-icon>
+            </v-btn>
+
+            <v-btn icon>
+                <v-icon>mdi-magnify</v-icon>
+            </v-btn>
+
+            <v-menu
+                    left
+                    bottom
+            >
+                <template v-slot:activator="{ on }">
+                    <v-btn icon v-on="on">
+                        <v-icon>mdi-dots-vertical</v-icon>
+                    </v-btn>
+                </template>
+
+                <v-list>
+
+                </v-list>
+            </v-menu>
+        </v-app-bar>
+    </div>
 </template>
 
 <script>
+  import test from './components/test'
 export default {
   name: 'app',
   components: {
+    test
   }
 }
 </script>
