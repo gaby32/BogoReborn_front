858e79ea2ee924cac7e6150dfac1efcf29f24b30
UmbrellaCorpAgent <cedrick.guillet@gmail.com>
2019-09-07 16:38:48 -0400

    Sync avec Gâbbé + fix conflicts

--- src/components/bottom_toolbar.vue
+++ src/components/bottom_toolbar.vue
@@ -1,13 +1,30 @@
 <template>
-    <div>
-
+    <div v-bottom-navigation
+        :value="activeOption">
+        <v-btn v-for="option in options">{{ option.label }}</v-btn>
     </div>
 </template>
-<link
+<link rel="component_specific_stylesheet" href="{{ componentCssLink }}">
 <script>
     export default {
-        name: "bottom_toolbar"
+        name: "bottom_toolbar",
+        computed: {
+            componentCssLink: function() {
+                return this.isDark ? 'src/style/bottom_toolbar_dark.css' : 'src/style/bottom_toolbar_light.css'
+            },
+            options: function() {
+                return this.$store.getters.toolBarOptions;
+            }
+        },
+        methods: {
+        },
+        data() {
+            return {
+                isDark: this.$store.dispatch("isDarkTheme")
+            }
+        }
     }
+
 </script>
 
 <style scoped>
--- src/store.js
+++ src/store.js
@@ -1,5 +1,30 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { user_type_datas_generator } from 'usable_datas'
 Vue.use(Vuex);
 
-export default new Vuex.Store({})
+export default new Vuex.Store({
+    state: {
+        status: '',
+        user: {
+            type: '',
+            lang: ''
+        },
+        darkTheme: false,
+        componentDisplayed: '',
+        options: user_type_datas_generator.getOptions()
+    },
+    mutations: {
+
+    },
+    actions: {
+        isDarkTheme() {
+            return this.state.darkTheme
+        }
+    },
+    getters: {
+        userType: this.state.user.type,
+        userLang: this.state.user.lang,
+        toolBarOptions: this.state.options
+    }
+})
