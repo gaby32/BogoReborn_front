7ef42a32c0b9550d8a621e93a8bc722146ef5346
gaby32 <39409503+gaby32@users.noreply.github.com>
2019-09-06 23:32:19 -0400

    reborn

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from 'vuetify'
+import store from './store'
+import router from './router'
+import Axios from 'axios'
 
-Vue.config.productionTip = false
-
+Vue.config.productionTip = false;
+Vue.prototype.$http = Axios;
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    el: '#app',
+    router,
+    vuetify,
+    store,
+    render: h => h(App)
+});
