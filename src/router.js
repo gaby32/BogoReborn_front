7032ba73a86592adf51b0300b2098e8a76ef5495
gaby32 <39409503+gaby32@users.noreply.github.com>
2019-09-07 12:41:50 -0400

    router et pages

--- src/router.js
+++ src/router.js
@@ -1,12 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Login from './pages/login'
+import Home from './pages/home'
 Vue.use(Router);
 
 let router = new Router({
 routes : [
     {
         path: '/',
-        name: 'Home',
+        name: 'Home'
+    },
+    {
+        path: '/login',
+        name: 'Login'
+    },
+    {
+
     }
 ]
 });
